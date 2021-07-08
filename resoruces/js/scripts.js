5ec230b2b05da3ef623dc878938fbac2066fa1fa
ctheil <caleb.theil@ucdenver.edu>
2021-07-08 08:30:12 -0600

    update - JS hmbg menue

--- resoruces/js/scripts.js
+++ resoruces/js/scripts.js
@@ -58,5 +58,15 @@ $(document).ready(function() {
           }, {
               offset: '50%'
           });
+
+           /* Mobile Nav */ 
+
+        $('.js--nav-icon').click(function() {
+            var nav = $('.js--main-nav');
+            var icon = $('.js--nav-icon i');
+            nav.slideToggle(200);
+        });
+
+
           
 });
